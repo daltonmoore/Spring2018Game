4f6505797a0176606f68668b97bf6f966be0a510
Dalton Moore <daltonraymoore@gmail.com>
2018-02-18 13:45:36 -0500

    Prototype 01

---
+++ pixelMan/Assets/Scripts/Player/PlayerControllerVer2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerVer2 : MonoBehaviour
+{
+    public Animator anim;
+    public static PlayerControllerVer2 playerController;
+
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerControllerVer2>();
+    }
+
+    void Start ()
+    {
+        try
+        {
+            if (anim == null)
+                anim = GetComponent<Animator>();
+            if (rigid == null)
+                rigid = GetComponent<Rigidbody2D>();
+        }
+        catch(Exception e)
+        {
+            print(e.StackTrace);
+        }
+	}
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        move(x, y);
+    }
+
+    void move(float x, float y)
+    {
+        anim.SetFloat("Horizontal", x);
+        anim.SetFloat("Vertical", y);
+        anim.SetBool("Moving", x != 0 || y != 0);
+
+        rigid.velocity = new Vector2(x, y);
+
+        if(x == 0 && y == 0)
+        {
+            rigid.velocity = Vector2.zero;
+        }
+    }
+}
