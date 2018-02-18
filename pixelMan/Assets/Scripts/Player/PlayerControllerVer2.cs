e0c745c0686d90e85bb261acfd3a6586ec80ca9a
Dalton Moore <daltonraymoore@gmail.com>
2018-02-18 17:26:48 -0500

    Sprite and movement updated

--- pixelMan/Assets/Scripts/Player/PlayerControllerVer2.cs
+++ pixelMan/Assets/Scripts/Player/PlayerControllerVer2.cs
@@ -6,13 +6,17 @@ using UnityEngine;
 public class PlayerControllerVer2 : MonoBehaviour
 {
     public Animator anim;
+    public String[] animStates = new String[4];
     public static PlayerControllerVer2 playerController;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    String facing = "forwards";
 
     private void Awake()
     {
         playerController = GetComponent<PlayerControllerVer2>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Start ()
@@ -35,6 +39,50 @@ public class PlayerControllerVer2 : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         move(x, y);
+        if (!anim.GetBool("Moving"))
+        {
+            setSpriteFacing();
+        }
+    }
+
+    void setSpriteFacing()
+    {
+        if (facing == "left")
+        {
+            anim.Play(animStates[0]);
+        }
+        else if (facing == "right")
+        {
+            anim.Play(animStates[1]);
+        }
+        else if (facing == "towards")
+        {
+            anim.Play(animStates[2]);
+        }
+        else if (facing == "forwards")
+        {
+            anim.Play(animStates[3]);
+        }
+    }
+
+    void setLastDirection(float x, float y)
+    {
+        if (anim.GetFloat("Horizontal") > 0)
+        {
+            facing = "right";
+        }
+        else if (anim.GetFloat("Horizontal") < 0)
+        {
+            facing = "left";
+        }
+        else if(anim.GetFloat("Vertical") < 0)
+        {
+            facing = "towards";
+        }
+        else if (anim.GetFloat("Vertical") > 0)
+        {
+            facing = "forwards";
+        }
     }
 
     void move(float x, float y)
@@ -49,5 +97,6 @@ public class PlayerControllerVer2 : MonoBehaviour
         {
             rigid.velocity = Vector2.zero;
         }
+        setLastDirection(x, y);
     }
 }
