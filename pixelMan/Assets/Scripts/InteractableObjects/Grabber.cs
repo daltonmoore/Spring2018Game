9cbf50317d6555ffca45c978116b0478cde39491
Scott-Buglass-GSU <scott_m_buglass@georgiasouthern.edu>
2018-03-02 15:19:06 -0500

    Added Rudimentary Dialog Boxes
    You heard me

--- pixelMan/Assets/Scripts/InteractableObjects/Grabber.cs
+++ pixelMan/Assets/Scripts/InteractableObjects/Grabber.cs
@@ -5,58 +5,31 @@ using UnityEngine;
 public class Grabber : MonoBehaviour
 {
     GameObject painting;
-    PaintingSlot paintingSlot;
-    bool inPaintingTrigger, hasPainting;
-    Sprite paintingSprite;
+    PlayerControllerVer2 p;
 
     // Use this for initialization
-    void Start()
+    void Start ()
+    {
+        	p = GetComponentInParent<PlayerControllerVer2>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
     {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        grab();
-    }
-
-    void grab()
-    {
-        if(Input.GetKeyDown(KeyCode.E) && inPaintingTrigger && !hasPainting)//you are in range to grab the painting and you press the grab button and you don't have a painting
-        {
-            painting.SetActive(false);
-        }
-        else if(Input.GetKeyDown(KeyCode.E)) //you press e 
-        {
-            print("cannot grab");
-        }
-    }
-
-    void placer()
-    {
-
-    }
+	}
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        print("entered");
         if (other.tag == "PaintingGrab")
         {
-            inPaintingTrigger = true;
-            painting = other.transform.parent.gameObject;
+            p.canGrab = true;
         }
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        p.canGrab = false;
     }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "PaintingGrab")
-        {
-            inPaintingTrigger = false;
-        }
-    }   
 }
---
+++ pixelMan/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int curLine;
+    public int endLine;
+
+    public PlayerController player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+
+
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if(endLine == 0)
+        {
+            endLine = textLines.Length - 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theText.text = textLines[curLine];
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            curLine++;
+            if (curLine > endLine)
+            {
+                textBox.SetActive(false);
+                curLine = 0;
+            }
+        }
+
+        
+    }
+}
