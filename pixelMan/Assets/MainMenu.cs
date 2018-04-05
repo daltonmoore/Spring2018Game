773ed64acf6d32ca5cfd38ea7cc92ca448e73635
JacquelineChambers <jacquelineschambers@gmail.com>
2018-04-05 13:51:37 -0400

    play button works

---
+++ pixelMan/Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//allows unity to change scene
+
+public class MainMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);//gets the scene dungeon 
+    }
+	
+}
