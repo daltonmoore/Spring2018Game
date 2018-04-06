cae313eece2d09276a250b688edd1481ef67193c
JacquelineChambers <jacquelineschambers@gmail.com>
2018-04-06 11:34:38 -0400

    some things work

---
+++ pixelMan/Assets/doorTriggerScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class doorTriggerScript : MonoBehaviour
+{
+
+
+
+    public void Library()
+    {
+        SceneManager.LoadScene(1);//gets the scene dungeon 
+    }
+}
